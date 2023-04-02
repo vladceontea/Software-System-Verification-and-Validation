f027ac4f2512125a0b390a904db9988ab2be619f
VladCeontea <ceonteavladcalin@yahoo.com>
2023-04-02 16:40:09 +0300

    Lab2_TH

--- src/test/java/AddStudentTest.java
+++ src/test/java/AddStudentTest.java
@@ -36,4 +36,63 @@ public class AddStudentTest {
         assertEquals(1, service.saveStudent("", "Ana", 933));
     }
 
+    @Test
+    public void testCase_3_nullId(){
+        assertEquals(1, service.saveStudent(null, "Ana", 933));
+    }
+
+    @Test
+    public void testCase_4_emptyName(){
+        assertEquals(1, service.saveStudent("7", "", 933));
+    }
+
+    @Test
+    public void testCase_5_nullName(){
+        assertEquals(1, service.saveStudent("8", null, 933));
+    }
+
+    @Test
+    public void testCase_6_smallGroup(){
+        assertEquals(1, service.saveStudent("9", "Ana", 105));
+    }
+
+    @Test
+    public void testCase_7_largeGroup(){
+        assertEquals(1, service.saveStudent("10", "Ana", 1000));
+    }
+
+    @Test
+    public void testCase_8_bva_LowerBound(){
+        assertEquals(1, service.saveStudent("11", "Vasile", 111));
+    }
+
+
+    @Test
+    public void testCase_9_bva_UpperBound(){
+        assertEquals(1, service.saveStudent("12", "Vasile", 937));
+    }
+
+    @Test
+    public void testCase_10_bva_UnderLowerBound(){
+        assertEquals(1, service.saveStudent("13", "Vasile", 110));
+    }
+
+
+    @Test
+    public void testCase_11_bva_AboveLowerBound(){
+        assertEquals(1, service.saveStudent("14", "Vasile", 112));
+    }
+
+    @Test
+    public void testCase_12_bva_UnderUpperBound(){
+        assertEquals(1, service.saveStudent("15", "Vasile", 936));
+    }
+
+
+    @Test
+    public void testCase_13_bva_AboveUpperBound(){
+        assertEquals(1, service.saveStudent("16", "Vasile", 938));
+    }
+
+
 }
