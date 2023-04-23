c9a6cf68ade2fa43106d4b84904c0c5c7523b67c
VladCeontea <ceonteavladcalin@yahoo.com>
2023-04-23 19:50:45 +0300

    Lab3_TH_updated

--- src/test/java/AddStudentTest.java
+++ src/test/java/AddStudentTest.java
@@ -4,15 +4,13 @@ import domain.Student;
 import domain.Tema;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
-import validation.NotaValidator;
-import validation.StudentValidator;
-import validation.TemaValidator;
-import validation.Validator;
+import validation.*;
 
 public class AddStudentTest {
 
@@ -26,72 +24,73 @@ public class AddStudentTest {
 
     Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
 
+    private static final String INVALID_ID_ERROR_MESSAGE = "ID invalid! \n";
+    private static final String INVALID_NAME_ERROR_MESSAGE = "Nume invalid! \n";
+    private static final String INVALID_GROUP_ERROR_MESSAGE = "Grupa invalida! \n";
+
     @Test
     public void testCase_1_valid(){
-        assertEquals(1, service.saveStudent("6", "Ana", 933));
+        assertEquals(0, service.saveStudent("6", "Ana", 933));
     }
 
     @Test
     public void testCase_2_emptyId(){
-        assertEquals(1, service.saveStudent("", "Ana", 933));
+        assertThrows(ValidationException.class, () -> service.saveStudent("", "Ana", 933), INVALID_ID_ERROR_MESSAGE);
     }
 
     @Test
     public void testCase_3_nullId(){
-        assertEquals(1, service.saveStudent(null, "Ana", 933));
+        assertThrows(ValidationException.class, () -> service.saveStudent(null, "Ana", 933), INVALID_ID_ERROR_MESSAGE);
     }
 
     @Test
     public void testCase_4_emptyName(){
-        assertEquals(1, service.saveStudent("7", "", 933));
+        assertThrows(ValidationException.class, () -> service.saveStudent("7", "", 933), INVALID_NAME_ERROR_MESSAGE);
     }
 
     @Test
     public void testCase_5_nullName(){
-        assertEquals(1, service.saveStudent("8", null, 933));
+        assertThrows(ValidationException.class, () -> service.saveStudent("8", null, 933), INVALID_NAME_ERROR_MESSAGE);
     }
 
     @Test
     public void testCase_6_smallGroup(){
-        assertEquals(1, service.saveStudent("9", "Ana", 105));
+        assertThrows(ValidationException.class, () -> service.saveStudent("9", "Ana", 105), INVALID_GROUP_ERROR_MESSAGE);
     }
 
     @Test
     public void testCase_7_largeGroup(){
-        assertEquals(1, service.saveStudent("10", "Ana", 1000));
+        assertThrows(ValidationException.class, () -> service.saveStudent("10", "Ana", 1000), INVALID_GROUP_ERROR_MESSAGE);
     }
 
     @Test
     public void testCase_8_bva_LowerBound(){
-        assertEquals(1, service.saveStudent("11", "Vasile", 111));
+        assertEquals(0, service.saveStudent("11", "Vasile", 111));
     }
-
 
     @Test
     public void testCase_9_bva_UpperBound(){
-        assertEquals(1, service.saveStudent("12", "Vasile", 937));
+        assertEquals(0, service.saveStudent("12", "Vasile", 937));
     }
 
     @Test
     public void testCase_10_bva_UnderLowerBound(){
-        assertEquals(1, service.saveStudent("13", "Vasile", 110));
+        assertThrows(ValidationException.class, () -> service.saveStudent("13", "Vasile", 110), INVALID_GROUP_ERROR_MESSAGE);
     }
-
 
     @Test
     public void testCase_11_bva_AboveLowerBound(){
-        assertEquals(1, service.saveStudent("14", "Vasile", 112));
+        assertEquals(0, service.saveStudent("14", "Vasile", 112));
     }
 
     @Test
     public void testCase_12_bva_UnderUpperBound(){
-        assertEquals(1, service.saveStudent("15", "Vasile", 936));
+        assertEquals(0, service.saveStudent("15", "Vasile", 936));
     }
-
 
     @Test
     public void testCase_13_bva_AboveUpperBound(){
-        assertEquals(1, service.saveStudent("16", "Vasile", 938));
+        assertThrows(ValidationException.class, () -> service.saveStudent("16", "Vasile", 938), INVALID_GROUP_ERROR_MESSAGE);
     }
 
 
